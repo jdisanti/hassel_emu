e85468f820c3e9264668d788ff79b1b1b60d6128
John DiSanti <johndisanti@gmail.com>
2017-12-26 16:58:50 -0800

    Add a peripheral bus

--- src/bus.rs
+++ src/bus.rs
@@ -1,3 +1,8 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use graphics_bus::GraphicsBus;
+
 pub trait Bus {
     // There's a mutable read for peripherals such as the PPU and Input
     // because reading some of their registers causes a state change.
@@ -31,6 +36,49 @@ impl Bus {
         let lsb = bus.read_byte_mut(addr as u16);
         let msb = bus.read_byte_mut(addr.wrapping_add(1) as u16);
         (msb as u16) << 8 | (lsb as u16)
+    }
+}
+
+const GRAPHICS_REGISTER_ADDRESS: u16 = 0xDFFE;
+const IO_REGISTER_ADDRESS: u16 = 0xDFFF;
+
+pub struct PeripheralBus {
+    graphics_bus: Rc<RefCell<GraphicsBus>>,
+}
+
+impl PeripheralBus {
+    pub fn new(graphics_bus: Rc<RefCell<GraphicsBus>>) -> PeripheralBus {
+        PeripheralBus {
+            graphics_bus: graphics_bus
+        }
+    }
+}
+
+impl Bus for PeripheralBus {
+    fn read_byte(&self, _addr: u16) -> u8 {
+        0
+    }
+
+    fn read_byte_mut(&mut self, addr: u16) -> u8 {
+        if addr == GRAPHICS_REGISTER_ADDRESS {
+            self.graphics_bus.borrow_mut().read_byte_mut(addr)
+        } else if addr == IO_REGISTER_ADDRESS {
+            // TODO
+            0
+        } else {
+            println!("WARN: PeripheralBus called with non-peripheral address 0x{:04X}", addr);
+            0
+        }
+    }
+
+    fn write_byte(&mut self, addr: u16, val: u8) {
+        if addr == GRAPHICS_REGISTER_ADDRESS {
+            self.graphics_bus.borrow_mut().write_byte(addr, val);
+        } else if addr == IO_REGISTER_ADDRESS {
+            // TODO
+        } else {
+            println!("WARN: PeripheralBus called with non-peripheral address 0x{:04X}", addr);
+        }
     }
 }
 
