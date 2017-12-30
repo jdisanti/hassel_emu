9cf2fcabd493fee005129a3896efc1538ad69bcf
John DiSanti <johndisanti@gmail.com>
2017-12-30 10:27:55 -0800

    Apply open source licenses

--- src/cpu/instruction/common.rs
+++ src/cpu/instruction/common.rs
@@ -1,3 +1,12 @@
+//
+// Copyright 2017 hassel_emu Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
 use bus::Bus;
 use cpu::opcode::{CpuAddressMode, OpAddressMode};
 use cpu::registers::Registers;
--- src/cpu/register_status.rs
+++ src/cpu/register_status.rs
@@ -1,3 +1,12 @@
+//
+// Copyright 2017 hassel_lib6502 Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
 const MASK_NEGATIVE: u8 = 0x80;
 const MASK_OVERFLOW: u8 = 0x40;
 const MASK_BIT_5: u8 = 0x20;
